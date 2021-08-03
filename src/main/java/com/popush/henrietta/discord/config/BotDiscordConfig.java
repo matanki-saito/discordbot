1171aa1837f8dddf5d86269a075c03ceaee1eee5
matanki.saito <matanki.saito@gmail.com>
2021-08-03 22:26:08 +0900

    Add retry to jda bean creation method

--- src/main/java/com/popush/henrietta/discord/config/BotDiscordConfig.java
+++ src/main/java/com/popush/henrietta/discord/config/BotDiscordConfig.java
@@ -1,7 +1,5 @@
 package com.popush.henrietta.discord.config;
 
-import javax.security.auth.login.LoginException;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +8,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
-import com.github.ygimenez.exception.InvalidHandlerException;
 import com.github.ygimenez.method.Pages;
 import com.github.ygimenez.model.PaginatorBuilder;
 import com.popush.henrietta.discord.Bot;
@@ -29,20 +26,27 @@ public class BotDiscordConfig {
     private String name;
 
     @Bean
-    public JDA beanJda() throws LoginException, InterruptedException, InvalidHandlerException {
-        Thread.sleep(5000);
+    public JDA beanJda() throws InterruptedException {
+        for (var i = 0; i < 20; i++) {
+            try {
+                var jda = JDABuilder.createDefault(discordToken)
+                                    .addEventListeners(bot)
+                                    .setActivity(Activity.playing(name))
+                                    .build();
 
-        var jda = JDABuilder.createDefault(discordToken)
-                            .addEventListeners(bot)
-                            .setActivity(Activity.playing(name))
-                            .build();
+                jda.awaitReady();
 
-        Pages.activate(PaginatorBuilder
-                               .createPaginator()
-                               .setHandler(jda)
-                               .shouldRemoveOnReact(true)
-                               .build());
+                Pages.activate(PaginatorBuilder
+                                       .createPaginator()
+                                       .setHandler(jda)
+                                       .shouldRemoveOnReact(true)
+                                       .build());
+                return jda;
 
-        return jda;
+            } catch (Exception e) {
+                Thread.sleep(5000);
+            }
+        }
+        throw new IllegalStateException();
     }
 }

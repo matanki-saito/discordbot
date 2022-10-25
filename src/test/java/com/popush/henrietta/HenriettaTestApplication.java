609f0cf11fc5061292ad3d4c5c8b9347b5093f62
matanki.saito <matanki.saito@gmail.com>
2022-10-25 23:03:23 +0900

    Bump up jdk version 15 -> 17 , Remove tests

--- src/test/java/com/popush/henrietta/HenriettaTestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.popush.henrietta;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-class HenriettaTestApplication {
-    /* 何も書かないこと */
-    /* http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-test-autoconfigure/ */
-}

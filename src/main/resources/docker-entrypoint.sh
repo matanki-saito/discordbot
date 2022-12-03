c0bb49b3bd655eaf3c265c5766ebadc2095ff5c2
matanki.saito <matanki.saito@gmail.com>
2022-12-04 06:34:54 +0900

    Add --enable-preview because to use java 17 preview feature

--- src/main/resources/docker-entrypoint.sh
+++ src/main/resources/docker-entrypoint.sh
@@ -11,4 +11,4 @@ fi
 
 printenv
 
-java -cp app:app/lib/* -Dspring.profiles.active=prod com.popush.henrietta.HenriettaApplication
+java --enable-preview -cp app:app/lib/* -Dspring.profiles.active=prod com.popush.henrietta.HenriettaApplication
